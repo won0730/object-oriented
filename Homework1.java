0133c04bec8eb45a7119f58452b7bb9c7c7b4d15
won0730 <jungwon0730@naver.com>
2025-09-16 15:38:45 +0900

    Add java File

---
+++ Homework1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("정수를 입력하세요: ");
+            int num = sc.nextInt();
+            sum += num;
+            System.out.printf("현재까지 입력된 정수의 합은 %d입니다.\n", sum);
+        }
+    }
+}
