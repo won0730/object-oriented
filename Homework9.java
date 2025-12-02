8996bd7b1fb218458247e02fb47014b74176f8a3
won0730 <jungwon0730@naver.com>
2025-12-02 13:38:41 +0900

    Add java File

---
+++ Homework9.java
@@ -0,0 +1,52 @@
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class Homework9 {
+    static Map<String, String> user = new HashMap<>();
+    static {
+        try {
+            FileReader fr = new FileReader("db.txt");
+            Scanner fs = new Scanner(fr);
+
+            while (fs.hasNext()) {
+                String id = fs.next();
+                String password = fs.next();
+                user.put(id, password);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("db.txt 파일을 찾을 수 없습니다.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean login = false;
+
+        while (!login) {
+            System.out.println("id와 password를 입력해주세요.");
+            System.out.print("id: ");
+            String inputId = sc.nextLine().trim();
+
+            if (!user.containsKey(inputId)) {
+                System.out.println("입력하신 id는 존재하지 않습니다. 다시 입력해주세요.\n");
+                continue;
+            }
+
+            System.out.print("password: ");
+            String inputPassword = sc.nextLine().trim();
+            String password = user.get(inputId);
+
+            if (!password.equals(inputPassword)) {
+                System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.\n");
+            }else {
+                System.out.println("id와 비밀번호가 일치합니다.");
+                login = true;
+            }
+        }
+    }
+}
